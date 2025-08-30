efa07efe7f91858caa42192fcdff4864d6f9c1f4
Liam Spatola <liam.spatola@gmail.com>
2025-08-30 14:30:45 +1000

    Added pages to navigate and updated nav bar

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 
+from utils.weather import Weather
+
 
 app = Flask(__name__)
 
@@ -8,6 +10,20 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
+@app.route("/current")
+def current():
+    return render_template("current.html")
+
+
+@app.route("/forecast")
+def forecast():
+    return render_template("forecast.html")
+
+
+@app.route("/astronomy")
+def astronomy():
+    return render_template("astronomy.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
