eeed3329fb6ae42fbe23ddc71a182f7f59eb60b5
Liam Spatola <liam.spatola@gmail.com>
2025-09-01 11:09:09 +1000

    Updated weather.py to use open-meteo.com data instead of wttr.in

--- utils/weather.py
+++ utils/weather.py
@@ -60,16 +60,16 @@ class Weather:
             "latitude": self.coords["lat"],
             "longitude": self.coords["lng"],
             "timezone": self.timezone,
-            "current": ",".join(current_params),
-            "daily": daily_params,
-            "hourly": hourly_params
+            "current": ",".join(current_params) if current_params else None,
+            "daily": ",".join(daily_params) if daily_params else None,
+            "hourly": ",".join(hourly_params) if hourly_params else None
         }
         response = requests.get(url, params=params)
         response.raise_for_status()
         return response.json()
         
     def get_current_weather(self):
-        data = self._get_open_meteo_json(["temperature_2m", "relative_humidity_2m", "dew_point_2m", "apparent_temperature", "precipitation_probability", "precipitation", "rain", "showers", "snowfall", "pressure_msl", "surface_pressure", "cloud_cover", "cloud_cover_low", "cloud_cover_mid", "cloud_cover_high", "visibility", "wind_speed_10m", "wind_gusts_10m", "wind_direction_10m", "uv_index"])
+        data = self._get_open_meteo_json(current_params=["temperature_2m", "relative_humidity_2m", "dew_point_2m", "apparent_temperature", "precipitation_probability", "precipitation", "rain", "showers", "snowfall", "pressure_msl", "surface_pressure", "cloud_cover", "cloud_cover_low", "cloud_cover_mid", "cloud_cover_high", "visibility", "wind_speed_10m", "wind_gusts_10m", "wind_direction_10m", "uv_index"])
 
         current_data = data["current"]
 
@@ -149,9 +149,123 @@ class Weather:
         return astronomy
     
     def get_forecast(self):
-        pass
+        hourly_data = self._get_open_meteo_json(hourly_params=["temperature_2m", "relative_humidity_2m", "dew_point_2m", "apparent_temperature", "precipitation_probability", "precipitation", "rain", "showers", "snowfall", "pressure_msl", "surface_pressure", "cloud_cover", "cloud_cover_low", "cloud_cover_mid", "cloud_cover_high", "visibility", "wind_speed_10m", "wind_gusts_10m", "wind_direction_10m", "uv_index"])
+        daily_data = self._get_open_meteo_json(daily_params=["temperature_2m_max", "temperature_2m_min", "temperature_2m_mean", "apparent_temperature_max", "apparent_temperature_min", "apparent_temperature_mean", "relative_humidity_2m_max", "relative_humidity_2m_min", "relative_humidity_2m_mean", "dew_point_2m_mean", "dew_point_2m_max", "dew_point_2m_min", "precipitation_probability_max", "precipitation_probability_min", "precipitation_probability_mean", "precipitation_hours", "precipitation_sum", "snowfall_sum", "showers_sum", "rain_sum", "pressure_msl_mean", "pressure_msl_max", "pressure_msl_min", "surface_pressure_mean", "surface_pressure_max", "surface_pressure_min", "cloud_cover_mean", "cloud_cover_max", "cloud_cover_min", "visibility_min", "visibility_max", "visibility_mean", "wind_speed_10m_min", "wind_gusts_10m_min", "wind_speed_10m_mean", "wind_gusts_10m_mean", "wind_speed_10m_max", "wind_gusts_10m_max", "winddirection_10m_dominant", "uv_index_max", "uv_index_clear_sky_max"])
+
+        daily_forecasts = []
+        # Processing the daily data
+        for idx, day in enumerate(daily_data["daily"]["time"]):
+            forecast = {
+                "date": day,
+                "temperature": {
+                    "temp_min": daily_data["daily"]["temperature_2m_min"][idx],
+                    "temp_mean": daily_data["daily"]["temperature_2m_mean"][idx],
+                    "temp_max": daily_data["daily"]["temperature_2m_max"][idx],
+                    "apparent_min": daily_data["daily"]["apparent_temperature_min"][idx],
+                    "apparent_mean": daily_data["daily"]["apparent_temperature_mean"][idx],
+                    "apparent_max": daily_data["daily"]["apparent_temperature_max"][idx]
+                },
+                "humidity": {
+                    "relative_min": daily_data["daily"]["relative_humidity_2m_min"][idx],
+                    "relative_mean": daily_data["daily"]["relative_humidity_2m_mean"][idx],
+                    "relative_max": daily_data["daily"]["relative_humidity_2m_max"][idx],
+                    "dewpoint_min": daily_data["daily"]["dew_point_2m_min"][idx],
+                    "dewpoint_mean": daily_data["daily"]["dew_point_2m_mean"][idx],
+                    "dewpoint_max": daily_data["daily"]["dew_point_2m_max"][idx]
+                },
+                "precipitation": {
+                    "probability_min": daily_data["daily"]["precipitation_probability_min"][idx],
+                    "probability_mean": daily_data["daily"]["precipitation_probability_mean"][idx],
+                    "probability_max": daily_data["daily"]["precipitation_probability_max"][idx],
+                    "total_hours": daily_data["daily"]["precipitation_hours"][idx],
+                    "total_sum": daily_data["daily"]["precipitation_sum"][idx],
+                    "rain_sum": daily_data["daily"]["rain_sum"][idx],
+                    "showers_sum": daily_data["daily"]["showers_sum"][idx],
+                    "snowfall_sum": daily_data["daily"]["snowfall_sum"][idx]
+                },
+                "pressure": {
+                    "sea_level_min": daily_data["daily"]["pressure_msl_min"][idx],
+                    "sea_level_mean": daily_data["daily"]["pressure_msl_mean"][idx],
+                    "sea_level_max": daily_data["daily"]["pressure_msl_max"][idx],
+                    "surface_min": daily_data["daily"]["surface_pressure_min"][idx],
+                    "surface_mean": daily_data["daily"]["surface_pressure_mean"][idx],
+                    "surface_max": daily_data["daily"]["surface_pressure_max"][idx]
+                },
+                "cloud_cover": {
+                    "min": daily_data["daily"]["cloud_cover_min"][idx],
+                    "mean": daily_data["daily"]["cloud_cover_mean"][idx],
+                    "max": daily_data["daily"]["cloud_cover_max"][idx]
+                },
+                "visibility": {
+                    "min": round(daily_data["daily"]["visibility_min"][idx] / 1000, 2),
+                    "mean": round(daily_data["daily"]["visibility_mean"][idx] / 1000, 2),
+                    "max": round(daily_data["daily"]["visibility_max"][idx] / 1000, 2)
+                },
+                "wind": {
+                    "min_speed": daily_data["daily"]["wind_speed_10m_min"][idx],
+                    "min_gusts": daily_data["daily"]["wind_gusts_10m_min"][idx],
+                    "mean_speed": daily_data["daily"]["wind_speed_10m_mean"][idx],
+                    "mean_gusts": daily_data["daily"]["wind_gusts_10m_mean"][idx],
+                    "max_speed": daily_data["daily"]["wind_speed_10m_max"][idx],
+                    "max_gusts": daily_data["daily"]["wind_gusts_10m_max"][idx],
+                    "direction": daily_data["daily"]["winddirection_10m_dominant"][idx]
+                },
+                "uv_index": {
+                    "index": daily_data["daily"]["uv_index_max"][idx],
+                    "clear_sky_index": daily_data["daily"]["uv_index_clear_sky_max"][idx]
+                }
+            }
+
+            daily_forecasts.append(forecast)
+
+            # Processing the hourly data
+            hourly_forecasts = []
+            for idx, hour in enumerate(hourly_data["hourly"]["time"]):
+                forecast = {
+                    "datetime": {
+                        "date": hourly_data["hourly"]["time"][idx].split("T")[0],
+                        "time": hourly_data["hourly"]["time"][idx].split("T")[1]
+                    },
+                    "temperature": {
+                        "air_temp": hourly_data["hourly"]["temperature_2m"][idx],
+                        "apparent_temp": hourly_data["hourly"]["apparent_temperature"][idx]
+                    },
+                    "humidity": {
+                        "relative": hourly_data["hourly"]["relative_humidity_2m"][idx],
+                        "dewpoint": hourly_data["hourly"]["dew_point_2m"][idx]
+                    },
+                    "precipitation": {
+                        "probability": hourly_data["hourly"]["precipitation_probability"][idx],
+                        "amount": hourly_data["hourly"]["precipitation"][idx],
+                        "rain": hourly_data["hourly"]["rain"][idx],
+                        "showers": hourly_data["hourly"]["showers"][idx],
+                        "snowfall": hourly_data["hourly"]["snowfall"][idx]
+                    },
+                    "pressure": {
+                        "sea_level": hourly_data["hourly"]["pressure_msl"][idx],
+                        "surface": hourly_data["hourly"]["surface_pressure"][idx]
+                    },
+                    "cloud_cover": {
+                        "total": hourly_data["hourly"]["cloud_cover"][idx],
+                        "low": hourly_data["hourly"]["cloud_cover_low"][idx],
+                        "mid": hourly_data["hourly"]["cloud_cover_mid"][idx],
+                        "high": hourly_data["hourly"]["cloud_cover_high"][idx]
+                    },
+                    "visibility": round(hourly_data["hourly"]["visibility"][idx] / 1000, 2),
+                    "wind": {
+                        "speed": hourly_data["hourly"]["wind_speed_10m"][idx],
+                        "gusts": hourly_data["hourly"]["wind_gusts_10m"][idx],
+                        "direction": hourly_data["hourly"]["wind_direction_10m"][idx]
+                    },
+                    "uv_index": hourly_data["hourly"]["uv_index"][idx]
+                }
+
+                hourly_forecasts.append(forecast)
+            
+            return (daily_forecasts, hourly_forecasts)
+
 
 if __name__ == "__main__":
     w = Weather()
-    w.city, w.coords, w.timezone = w.get_location_by_name("sydney")
+    w.city, w.coords, w.timezone = w.get_location_by_user_ip
     print(w.get_current_weather())
